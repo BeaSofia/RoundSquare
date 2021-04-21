8499d0a24b1a5e04a264fbb2ce74885019f4c5e0
BeaSofia <beatrizsofiarb@live.com.pt>
2021-04-21 20:24:44 +0100

    dia 21.04

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,10 @@
 import pt.isel.canvas.*
-import java.awt.Color
 
 val RANGE_SIDE = 10..400
 val RANGE_ROUND = 0.0..1.0
 val side = 200
 val round = 0.5
-val f = ((side/2) * round ).toInt()
+
 
 
 
@@ -13,50 +12,55 @@ val f = ((side/2) * round ).toInt()
 data class Position (val x: Int, val y: Int)
 data class RoundSquare (val center: Position, val side: Int, val round: Double, val color:Int)
 
-fun drawObject (rs: RoundSquare,cv: Canvas): RoundSquare{
+fun drawObject (rs: RoundSquare,cv: Canvas){
+    val f = ((rs.side/2)*rs.round).toInt()
+    cv.erase()
+    cv.drawRect(rs.center.x - (rs.side-2*f)/2, rs.center.y - rs.side/2,rs.side -2 * f,rs.side, GREEN )
+    cv.drawRect(rs.center.x - rs.side/2, rs.center.y - (rs.side/2-f),rs.side,rs.side -2 * f, GREEN )
+    cv.drawCircle(rs.center.x - (rs.side/2) + f, rs.center.y - (rs.side/2) + f, f, GREEN )
+    cv.drawCircle(rs.center.x + (rs.side/2) - f, rs.center.y - (rs.side/2) + f, f, GREEN )
+    cv.drawCircle(rs.center.x + (rs.side/2) - f, rs.center.y + (rs.side/2) - f, f, GREEN )
+    cv.drawCircle(rs.center.x - (rs.side/2) + f, rs.center.y + (rs.side/2) - f, f, GREEN )
+    cv.drawText(5, 395,
+        "center = ( ${rs.center.x}, ${rs.center.y} )  side = $side round = ${(round * 100).toInt()}% color = 0x${rs.color.toString(16)}",
+        BLACK, 12)
 
-    cv.drawRect(cv.width/2 - (side-2 *f)/2, cv.height/2 - side/2,side -2 * f,side, GREEN )
-    cv.drawRect(cv.width/2 - side/2, cv.height/2 - (side/2-f),side,side -2 * f, GREEN )
-    cv.drawCircle(cv.width/2 - f, cv.height/2 - f, f, GREEN )
-    cv.drawCircle(cv.width/2 + f, cv.height/2 - f, f, GREEN )
-    cv.drawCircle(cv.width/2 - f, cv.height/2 + f, f, GREEN )
-    cv.drawCircle(cv.width/2 + f, cv.height/2 + f, f, GREEN )
-    //cv.drawCircle(cv.width/2, cv.height/2, 2*f, RED)
-    return RoundSquare(rs.center, rs.side, rs.round, rs.color)
 }
 
-fun resise(rs: RoundSquare, cv: Canvas, ke: Char) : RoundSquare {
+
+fun newSize(rs: RoundSquare, ke: Char) : RoundSquare {
+
     val newRs: RoundSquare
     when(ke) {
 
-        'S' -> newRs = if (rs.side < 400) RoundSquare(rs.center, rs.side + 1, rs.round, rs.color) else rs
-        's' -> newRs = if (rs.side > 10) RoundSquare(rs.center, rs.side - 1, rs.round, rs.color) else rs
-        'R' -> newRs = if (rs.side < 0.0) RoundSquare(rs.center, rs.side , rs.round + 1, rs.color) else rs
-        'r' -> newRs = if (rs.side > 1.0) RoundSquare(rs.center, rs.side , rs.round- 1, rs.color) else rs
+         'S' -> newRs = if (rs.side < 400) RoundSquare(rs.center, rs.side + 1, rs.round, rs.color) else rs
+         's' -> newRs = if (rs.side > 10) RoundSquare(rs.center, rs.side - 1, rs.round, rs.color) else rs
+         'R' -> newRs = if (rs.round < 1.0) RoundSquare(rs.center, rs.side , rs.round + 0.01, rs.color) else rs
+         'r' -> newRs = if (rs.round > 0.0) RoundSquare(rs.center, rs.side , rs.round - 0.01, rs.color) else rs
         else -> newRs = rs}
     return newRs
 }
 
-fun main(){
-    onStart{
 
-        val cv = Canvas(600,400)
-        var rs = RoundSquare(Position(cv.width/2, cv.height/2), side, round, GREEN)
-        drawObject(rs,cv)
+fun main() {
+    onStart {
 
-        cv.onTimeProgress(20) {
-            drawObject(rs,cv)
+        val cv = Canvas(600, 400)
+        var rs = RoundSquare(Position(cv.width / 2, cv.height / 2), side, round, GREEN)
+        drawObject(rs, cv)
+
+        cv.onKeyPressed { key: KeyEvent ->
+            rs = newSize(rs, key.char)
+            drawObject(rs, cv)
         }
 
-        cv.onKeyPressed { ke:KeyEvent ->
-            cv.erase()
-            rs = resise(rs, cv,ke.char)
-            drawObject(rs,cv)
+        cv.onMouseDown{
 
-
+            rs = RoundSquare( Position(it.x, it.y), rs.side, rs.round, rs.color)
+            drawObject(rs, cv)
         }
-
-
 
     }
+    onFinish {}
 }
+
