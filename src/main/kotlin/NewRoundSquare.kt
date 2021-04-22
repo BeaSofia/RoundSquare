9f6db83b0f906bbb0bacb5ef571604436e473358
BeaSofia <beatrizsofiarb@live.com.pt>
2021-04-22 18:20:04 +0100

    dia 22 - dicas do stor com comentários

--- src/main/kotlin/NewRoundSquare.kt
+++ src/main/kotlin/NewRoundSquare.kt
@@ -1,5 +1,7 @@
 import pt.isel.canvas.*
 
+
+/** Function that draws the RoundSquare and the information about it on the canvas**/
 fun drawRoundSquare (rs: RoundSquare, cv: Canvas){
     val f = ((rs.side/2)*rs.round).toInt()
     cv.erase()                              //We need to erase the canvas every time we use this function for the text not overwrite
@@ -14,7 +16,7 @@ fun drawRoundSquare (rs: RoundSquare, cv: Canvas){
         BLACK, 16)
 }
 
-
+/** Function that implements the modifications made based on the selected keys **/
 fun newSize(rs: RoundSquare, ke: Char): RoundSquare {
 
     return when (ke) {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import pt.isel.canvas.*
 
+/** Is the range of size and round values **/
 val RangeSide = 11..399
 val RangeRound = 0.0..1.0
 const val side = 200            // We use a const val because it's a value of a read-only property
@@ -12,16 +13,17 @@ data class RoundSquare (val center: Position, val side: Int, val round: Double, 
 
 fun main() {
     onStart {
-
+        /** Creation of the Canvas window and the first RoundSquare **/
         val cv = Canvas(600, 400)
         var rs = RoundSquare(Position(cv.width / 2, cv.height / 2), side, round, GREEN)
         drawRoundSquare(rs, cv)
 
+        /** Modifications based on the allowed keys **/
         cv.onKeyPressed { key: KeyEvent ->
             rs = newSize(rs, key.char)
             drawRoundSquare(rs, cv)
         }
-
+        /** The new position given by the user's mouse click  **/
         cv.onMouseDown{
 
             rs = RoundSquare( Position(it.x, it.y), rs.side, rs.round, rs.color)
